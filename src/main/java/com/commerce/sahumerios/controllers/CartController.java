5dbb597169f2baf5e097b6ff52fba92aea7f347e
dcirer <domy.741@gmail.com>
2024-07-22 22:32:03 -0300

    Modificacion de CartController getCarts

--- src/main/java/com/commerce/sahumerios/controllers/CartController.java
+++ src/main/java/com/commerce/sahumerios/controllers/CartController.java
@@ -12,7 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/carts")
@@ -25,28 +27,31 @@ public class CartController {
 
     //Metodo para agregar productos al carrito,
     // ademas verifica que no se agregue al carrito una cantidad que no hay en stock.
-    @PostMapping("/{clientId}/{productId}")
-    public ResponseEntity<Cart> addProduct(@PathVariable Long clientId, @PathVariable Long productId, @Valid @RequestBody Cart cart){
+    @PostMapping("/{clientId}/{productId}/{quantity}")
+    public ResponseEntity<Cart> addProduct(@PathVariable Long clientId, @PathVariable Long productId, @PathVariable Integer quantity){
         try {
             Optional<Client> optionalClient = clientsService.readOne(clientId);
             Optional<Product> optionalProduct = productsService.readOne(productId);
             if (optionalClient.isPresent() && optionalProduct.isPresent()){
-                Product foundProduct= optionalProduct.get();
-                if (cart.getAmoun()> foundProduct.getStock()){
+                Product foundProduct = optionalProduct.get();
+                if (quantity > foundProduct.getStock()){
                     return ResponseEntity.badRequest().body(null);
                 }
+                Cart cart = new Cart();
                 cart.setClient(optionalClient.get());
-                cart.setProduct(optionalProduct.get());
+                cart.setProduct(foundProduct);
+                cart.setAmoun(quantity);
+                cart.setPrice(foundProduct.getPrice());
+                cart.setBilled(false);
                 Cart savedCart = cartsService.save(cart);
                 return ResponseEntity.ok(savedCart);
-            }else {
+            } else {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
             }
-        }catch (Exception e){
+        } catch (Exception e){
             System.out.println(e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-
     }
     //metodo que elimina un producto del carrito.
     @DeleteMapping("/{id}")
@@ -59,6 +64,19 @@ public class CartController {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
             }
         }catch (Exception e){
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/{clientId}")
+    public ResponseEntity<List<Cart>> getCarts(@PathVariable Long clientId){
+        try {
+            List<Cart> carts = cartsService.readAll().stream()
+                    .filter(cart -> cart.getClient().getId().equals(clientId) && !cart.isBilled())
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok(carts);
+        } catch (Exception e){
             System.out.println(e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
