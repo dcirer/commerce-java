9976323fda158cb1b9686f08d6984763957c0300
dcirer <domy.741@gmail.com>
2024-07-27 15:41:12 -0300

    solucion a temas de dependencia swagger

--- src/main/java/com/commerce/sahumerios/SahumeriosApplication.java
+++ src/main/java/com/commerce/sahumerios/SahumeriosApplication.java
@@ -1,9 +1,12 @@
 package com.commerce.sahumerios;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
 public class SahumeriosApplication {
 
 	public static void main(String[] args) {
